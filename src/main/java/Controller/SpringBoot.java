ee74abd7c5942e870b783e27b0b0ae5cdc8b8933
sagar <sagar@gmail.com>
2024-07-20 09:27:48 +0530

    no change

--- src/main/java/Controller/SpringBoot.java
+++ src/main/java/Controller/SpringBoot.java
@@ -1,10 +1,12 @@
 package Controller;
 
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.GetMapping;
 
 
 @RestController
+@SpringBootApplication
 public class SpringBoot {
     @GetMapping("/wellcome")
    public String getmessage() {
